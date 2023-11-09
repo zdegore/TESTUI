b0697bc31ba69e22ee131da7cc3c83a32e8d85f0
meb291 <meb291@pitt.edu>
2023-11-08 23:04:44 -0500

    updated UI graphics and tried to make fuel data a graph

--- app/src/main/java/com/example/testui/fragments/FuelDataFragment.kt
+++ app/src/main/java/com/example/testui/fragments/FuelDataFragment.kt
@@ -1,16 +1,39 @@
 package com.example.testui.fragments
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.testui.R
+import com.example.testui.fragments.adapters.viewPagerAdapter
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+
+
 class FuelDataFragment : Fragment(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val graph2: GraphView = view.findViewById(R.id.idGraphView1)
+        val series = LineGraphSeries(
+            arrayOf(
+                DataPoint(0.0, 1.0),
+                DataPoint(1.0, 5.0),
+                DataPoint(2.0, 3.0),
+                DataPoint(3.0, 2.0),
+                DataPoint(4.0, 6.0)
+            )
+        )
+        graph2.addSeries(series)
     }
 
     override fun onCreateView(
@@ -20,6 +43,8 @@ class FuelDataFragment : Fragment(){
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fueldata, container, false)
     }
+
+
 
 
 }
