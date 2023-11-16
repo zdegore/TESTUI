403b307c91494a80c7a11735f2cbc25675cb7bd6
zdegore <zad20@pitt.edu>
2023-11-15 21:57:46 -0500

    update

--- app/src/main/java/com/example/testui/MainActivity.kt
+++ app/src/main/java/com/example/testui/MainActivity.kt
@@ -2,36 +2,59 @@ package com.example.testui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.example.testui.databinding.ActivityMainBinding
 import com.example.testui.fragments.Monitor1Fragment
 import com.example.testui.fragments.FuelDataFragment
 import com.example.testui.fragments.adapters.viewPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
 
 class MainActivity : AppCompatActivity(){
 
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var tabLayout: TabLayout
+    private lateinit var adapter: viewPagerAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setUpTabs()
+
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager2 = findViewById(R.id.viewPager2)
+
+        adapter = viewPagerAdapter(supportFragmentManager, lifecycle)
+        tabLayout.addTab(tabLayout.newTab().setText("First"))
+        tabLayout.addTab(tabLayout.newTab().setText("Second"))
+        tabLayout.addTab(tabLayout.newTab().setText("Third"))
+        viewPager2.adapter = adapter
+
+        tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    viewPager2.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+
+        })
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+
+            }
+
+        })
     }
-    private fun setUpTabs(){
-
-        val adapter = viewPagerAdapter(supportFragmentManager)
-        adapter.addFragment(Monitor1Fragment(), "Monitor")
-        adapter.addFragment(FuelDataFragment(), "Fuel Data")
-
-        binding.viewPager.adapter = adapter
-        binding.tabs.setupWithViewPager(binding.viewPager)
-
-        binding.tabs.getTabAt(0)!!.setIcon(R.drawable.monitor)
-        binding.tabs.getTabAt(1)!!.setIcon(R.drawable.baseline_thermostat_24)
-
-
-    }
-
-
 }
--- app/src/main/java/com/example/testui/fragments/Monitor1Fragment.kt
+++ app/src/main/java/com/example/testui/fragments/Monitor1Fragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.testui.R
 class Monitor1Fragment : Fragment(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -17,8 +20,21 @@ class Monitor1Fragment : Fragment(){
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_monitor1, container, false)
+        val view = inflater.inflate(R.layout.fragment_monitor1, container, false)
+        val spinner: Spinner = view.findViewById(R.id.spinner)
+
+        val items = arrayOf("CorePower & Burnup", "Core Power: ", "Average Tin:", "Soluble Boron:", "Cycle Burnup:", "Max PIn Burnup:")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, items)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        spinner.setSelection(0)
+
+
+
+        return view
     }
 
 
